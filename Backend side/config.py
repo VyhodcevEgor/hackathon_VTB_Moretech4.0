b6993e3f30823cfca0f3b12a14d25095e1e44832
Egor <121723410+VyhodcevEgor@users.noreply.github.com>
2022-12-31 18:37:22 +0300

    Delete config.py

--- Backend side/config.py
+++
@@ -1,6 +0,0 @@
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 465
-MAIL_USE_SSL = True
-MAIL_USERNAME = 'nutplayer1337@gmail.com'
-MAIL_DEFAULT_SENDER = 'nutplayer1337@gmail.com'
-MAIL_PASSWORD = 'dkkrkvqvowbahkss'
